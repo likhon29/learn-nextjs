228f8f5a4635c121ba88b89de851353ed84a47c4
likhon29 <mdabdurrouf.likhon@gmail.com>
2023-06-21 11:01:07 +0600

    server and client componets

--- src/app/(with-layout)/blogs/[...segments]/page.js
+++ src/app/(with-layout)/blogs/[...segments]/page.js
@@ -1,13 +1,19 @@
+"use client";
 
-const SingleBlog = ({params}) => {
-    console.log(params)
-    const [year,id]=params.segments 
-    return (
-        <div>
-            <h1>Single Blog:{year}</h1>
-            <h1> Blog:{id}</h1>
-        </div>
-    );
+import { useParams, useSearchParams } from "next/navigation";
+
+const SingleBlog = ({ params, searchParams }) => {
+  const [year, id] = params.segments;
+  const params2 = useParams();
+  const searchParams2 = useSearchParams();
+  console.log(searchParams2.toString());
+  return (
+    <div>
+      <h1>Title:{searchParams.title}</h1>
+      <h1>Single Blog:{year}</h1>
+      <h1> Blog:{id}</h1>
+    </div>
+  );
 };
 
 export default SingleBlog;
--- src/app/(with-layout)/blogs/page.js
+++ src/app/(with-layout)/blogs/page.js
@@ -1,59 +1,74 @@
+'use client'
 import Link from "next/link";
+import { useRouter } from "next/navigation";
 
 export const metadata = {
   title: "Blogs | Learn NextJs",
   description: "asdjkasdlkjasd",
 };
 const BlogsPage = () => {
-  const blogs =[
+  const blogs = [
     {
-      id:1,
-      title:'title-1',
-      year:2016,
-      description:'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatum quisquam similique, suscipit consectetur laudantium quaerat vero laborum eaque quos facere.'
+      id: 1,
+      title: "title-1",
+      year: 2016,
+      description:
+        "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatum quisquam similique, suscipit consectetur laudantium quaerat vero laborum eaque quos facere.",
     },
     {
-      id:2,
-      title:'title-2',
-      year:2019,
-      description:'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatum quisquam similique, suscipit consectetur laudantium quaerat vero laborum eaque quos facere.'
+      id: 2,
+      title: "title-2",
+      year: 2019,
+      description:
+        "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatum quisquam similique, suscipit consectetur laudantium quaerat vero laborum eaque quos facere.",
     },
     {
-      id:3,
-      title:'title-3',
-      year:2017,
-      description:'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatum quisquam similique, suscipit consectetur laudantium quaerat vero laborum eaque quos facere.'
+      id: 3,
+      title: "title-3",
+      year: 2017,
+      description:
+        "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatum quisquam similique, suscipit consectetur laudantium quaerat vero laborum eaque quos facere.",
     },
     {
-      id:4,
-      title:'title-4',
-      year:2018,
-      description:'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatum quisquam similique, suscipit consectetur laudantium quaerat vero laborum eaque quos facere.'
+      id: 4,
+      title: "title-4",
+      year: 2018,
+      description:
+        "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatum quisquam similique, suscipit consectetur laudantium quaerat vero laborum eaque quos facere.",
     },
     {
-      id:5,
-      title:'title-5',
-      year:2020,
-      description:'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatum quisquam similique, suscipit consectetur laudantium quaerat vero laborum eaque quos facere.'
+      id: 5,
+      title: "title-5",
+      year: 2020,
+      description:
+        "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatum quisquam similique, suscipit consectetur laudantium quaerat vero laborum eaque quos facere.",
     },
-    
-  ]
-    return (
-        <div className="text-[orange] container mx-auto">
-          
-            {/* {
+  ];
+  const router = useRouter()
+  return (
+    <div className="text-[orange] container mx-auto">
+      {/* {
               blogs.map(({id,title,year,description})=>{(<Link className="block border-blue-500 p-2 my-2" href={`/blogs/${year}/${id}`} key={id}>{title}</Link>)})
             } */}
-            {
-              blogs.map((blog)=><Link className="block border p-2 my-2" href={{
-                pathname:`/blogs/${blog.year}/${blog.id}`,
-                query:{
-                  title:blog.title
-                }
-              }} key={blog.id}>{blog.title}</Link>)
-            }
-        </div>
-    );
+      {blogs.map((blog) => (
+        <Link
+          className="block border p-2 my-2"
+          href={{
+            pathname: `/blogs/${blog.year}/${blog.id}`,
+            query: {
+              title: blog.title,
+            },
+          }}
+          key={blog.id}
+        >
+          {blog.title}
+        </Link>
+        // <button key={blog.id} className="block border p-2 my-2" onClick={()=>{
+        //   router.push(`/blogs/${blog.year}/${blog.id}?title=${blog.title}`);
+        // }}>{blog.title}</button>
+      ))}
+    </div>
+  );
 };
 
 export default BlogsPage;

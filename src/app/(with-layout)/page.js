d45cd0e0b469e69562d37886725f807b504f5d8d
likhon29 <mdabdurrouf.likhon@gmail.com>
2023-06-21 16:07:00 +0600

    image optimization

--- src/app/(with-layout)/page.js
+++ src/app/(with-layout)/page.js
@@ -1,9 +1,19 @@
-import Image from 'next/image'
-
-export default function Home() {
+import Image from "next/image";
+import React from "react";
+import image1 from "@/assets/tree-736885_1280.jpg";
+const HomePage = () => {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-    <h2>Hello NextJS</h2>
-    </main>
-  )
-}
+    <div className="container mx-auto">
+      <Image className=" mx-auto" src={image1} alt="" placeholder="blur"/>
+      {/* <Image
+        className="my-5 mx-auto"
+        width='1000'
+        height='600'
+        src="https://cdn.pixabay.com/photo/2017/02/08/17/24/fantasy-2049567_1280.jpg"
+        alt=""
+      /> */}
+    </div>
+  );
+};
+
+export default HomePage;
